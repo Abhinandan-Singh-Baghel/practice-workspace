909f80f94aa47a556ce4bf65cf35d08600762c89
Abhinandan Singh Baghel <abhinandansinghbaghel2001@gmail.com>
2023-07-07 16:26:02 +0530

    minor changes

--- spotifyapi.js
+++ spotifyapi.js
@@ -1,5 +1,5 @@
 // Authorization token that must have been created previously. See : https://developer.spotify.com/documentation/web-api/concepts/authorization
-const token = 'BQDi0LGnP9RUjgtjyKtsHPgS52ybRSYka0GFyH9nup7PoxgxMIoxeHcxp5I-LSGPOxcQSf2htkphQ9b2TTZIzEZPU2dfb9WeXhMTeodcjqmzY1nl-VPBVzZg22I5bh4oDC2TGqOKSXbuTlHVhIxlUGj5zbQxbX9sbQWTILutahfXme13xgeQ-bDbU2mlJMFRI3q3rK-62yKCtM7RbpRZA8uKIwQPsKEE7VX1BR7fdksDMTdWxmnj0A0-SrRcJovhgp0CS1wgcCscV1GilQxAo7N4';
+const token = 'write your token here ' //
 async function fetchWebApi(endpoint, method, body) {
   const res = await fetch(`https://api.spotify.com/${endpoint}`, {
     headers: {
